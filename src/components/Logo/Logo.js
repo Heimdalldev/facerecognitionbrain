210352ac1d4b42960053861ac2112519e8961f58
Daniel Lewis <daniel.lewis@heimdalldev.com>
2024-02-26 01:04:26 +0100

    Created project. Added Logo and background color. Tilt.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import brain from './Brain.png';
+import './Logo.css';
+
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+
+            <Tilt className='Tilt br-100 shadow-2' options={{ max : 55}} style={{  height: 250, width: 250 }}>
+                <div className='Tilt-inner pa3'><img style={{paddingTop: '60px'}} alt='Logo' src={brain}/></div>
+            </Tilt>
+        </div>
+    )
+}
+
+
+export default Logo;
