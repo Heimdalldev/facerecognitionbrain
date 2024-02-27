b0445099083ad7c8eb2eafb4c5b078cd902d554c
Daniel Lewis <daniel.lewis@heimdalldev.com>
2024-02-27 23:26:47 +0100

    started working on adding the face recognition api.

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FaceRecognition = () => {
+    return (
+        <div className="center ma">
+            <div className="absolute mt2">
+                <img alt="" src={''} width="500px" height="auto" />
+            </div>
+        </div>
+    )
+}
+
+
+export default FaceRecognition
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3 near-black'>
@@ -9,8 +9,11 @@ const ImageLinkForm = () => {
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input className='f4 pa2 w-70 center' type='text' />
-                    <button className="w-30 grow f4 link ph3 pv2 dib yellow bg-near-black">Detect</button>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+                    <button 
+                        className="w-30 grow f4 link ph3 pv2 dib yellow bg-near-black"
+                        onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>    
             </div>
         </div>
